bb57889e138ba3d35f921f0933adb35f3141c727
crahardja <89956931+crahardja@users.noreply.github.com>
2021-09-02 13:46:57 +0800

    Add files via upload

---
+++ 2-docker/config/settings.py
@@ -0,0 +1,5 @@
+NAME = "testUser2"
+#DATABASE_URL="postgresql://admin:admin@172.16.0.141:5432/sea-bootcamp"
+DATABASE_URL="postgresql://admin:admin@pg.sea-leaderboard:5432/sea-bootcamp"
+SQLALCHEMY_DATABASE_URI=DATABASE_URL
+SQLALCHEMY_TRACK_MODIFICATIONS=False
